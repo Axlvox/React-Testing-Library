825f7bd3598f9e642479ebf802a37375c582440f
Axlvox <leandro.jlds@gmail.com>
2023-07-05 06:16:15 -0300

    Requisito 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,12 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste o componente <NotFound.tsx />', () => {
+  renderWithRouter(<App />, { route: '/something-else' });
+
+  expect(screen.getByRole('heading', { name: 'Page requested not found' })).toBeInTheDocument();
+
+  const pokedexImage = screen.getByRole('img');
+  expect(pokedexImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
