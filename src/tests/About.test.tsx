d9199478b3eb93e981b69b1834d5995ca084f8f1
Axlvox <leandro.jlds@gmail.com>
2023-07-05 05:19:29 -0300

    Requisito 2

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,12 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste o componente <About.tsx />', () => {
+  renderWithRouter(<App />, { route: '/about' });
+
+  expect(screen.getByRole('heading', { name: 'About Pokédex' })).toBeInTheDocument();
+
+  const pokedexImage = screen.getByRole('img');
+  expect(pokedexImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
