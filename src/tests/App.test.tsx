da3604d6fad858d7ff004aee45bdf5e403450ad8
Axlvox <leandro.jlds@gmail.com>
2023-07-05 03:54:26 -0300

    Requisito 1

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,11 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste o componente <App.tsx />', () => {
+  const { user } = renderWithRouter(<App />);
+
+  expect(screen.getByText('Home')).toBeInTheDocument();
+  expect(screen.getByText('About')).toBeInTheDocument();
+  expect(screen.getByText('Favorite Pokémon')).toBeInTheDocument();
+});
