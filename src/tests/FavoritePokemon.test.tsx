8101e53c52d810f6d45efd0eb7fe3c73318bb3de
Axlvox <leandro.jlds@gmail.com>
2023-07-05 06:11:50 -0300

    Requisito 3

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,32 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+import App from '../App';
+
+test('Testa se há a mensagem "No favorite pokemon found"', () => {
+  const mockPokemonList = [];
+  renderWithRouter(
+    <FavoritePokemon pokemonList={ mockPokemonList } />,
+  );
+
+  expect(screen.getByText(/No favorite Pokémon found/i)).toBeInTheDocument();
+});
+
+test('Teste o componente <FavoritePokemon /> com Pokémon favoritados', async () => {
+  renderWithRouter(<App />);
+
+  const detailsLink = screen.getByRole('link', { name: /more details/i });
+  await userEvent.click(detailsLink);
+
+  const favoritePokemon = screen.getByText(/pokémon favoritado\?/i);
+  await userEvent.click(favoritePokemon);
+
+  const favoriteLink = screen.getByRole('link', { name: /favorite pokémon/i });
+  await userEvent.click(favoriteLink);
+
+  renderWithRouter(<FavoritePokemon />);
+
+  const pikachuImage = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+  expect(pikachuImage).toBeInTheDocument();
+});
